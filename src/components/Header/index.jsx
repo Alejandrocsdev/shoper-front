5900c5b25df4fdfdaee27a93ee52417ceb566168
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-25 17:53:20 +0800

    add Header component (basic styling)
    - add light & dark versions of logo in assets folder.
    - remove react default styling.

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import Styles from './style.module.css'
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <header className={Styles.header}>
+        <nav className={Styles.nav}>
+          <div className={Styles.navLeft}>Nav Left</div>
+          <div className={Styles.navRight}>Nav Right</div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Header
