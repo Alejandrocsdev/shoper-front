3f63b4da6dcbbe1dcc97e891ea1a35d6e4ed43ff
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-07-13 18:03:39 +0800

    test cookie

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,13 @@
 // Module Style
 import Styles from './style.module.css'
+import useAuth from '../../hooks/useAuth'
 
 function Home() {
+  const { auth } = useAuth()
+  console.log(auth?.accessToken)
   return (
     <>
-      <main className={Styles.main}></main>
+      <main className={Styles.main}>{auth?.accessToken}</main>
     </>
   )
 }
